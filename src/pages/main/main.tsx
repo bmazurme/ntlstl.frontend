e589c0f1566d7349772ce3a53f3095f78383ea65
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-03 21:57:05 +0300

    feat: main-page

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Content from '../../components/content';
+import MainLayout from '../../layouts/main';
+
+export default function MainPage() {
+  return (
+    <Content header>
+      <MainLayout />
+    </Content>
+  );
+}
