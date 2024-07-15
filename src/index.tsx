96e2ed31c1c72d5fb2bd9a7dfb98d21318b7d939
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-15 22:13:27 +0300

    feat: store & router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './app';
+import { store } from './store';
 import reportWebVitals from './reportWebVitals';
-
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
 
 import './index.css';
 
@@ -16,7 +14,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
