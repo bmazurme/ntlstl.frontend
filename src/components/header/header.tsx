8eadcaf2da3303cc7780c2cb298e7d37e27e8830
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-17 19:41:44 +0300

    fix: header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { styled } from '@mui/material/styles';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import { Toolbar } from '@mui/material';
+import { Button, Stack, Toolbar } from '@mui/material';
+
+import { useAppLocation } from '../../hooks';
 
 import {
   Logo, MenuButton, ProfileMenu, ThemeButton, NotificationsIconButton,
@@ -35,16 +38,45 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 export default function Header() {
+  const { pathname } = useAppLocation();
+  const navigate = useNavigate();
   const { isOpen } = useContext(DrawerContext);
+  const handleNavigate = (url: string) => navigate(url);
 
   return (
-    <AppBar position="fixed" open={isOpen}>
+    <AppBar color="default" position="fixed" open={isOpen}>
       <Toolbar>
         <MenuButton />
         <Logo />
-        <NotificationsIconButton />
-        <ProfileMenu />
-        <ThemeButton />
+        <Stack spacing={1} direction="row">
+          <Button
+            size="small"
+            variant={pathname === '/posts' ? 'contained' : 'text'}
+            onClick={() => handleNavigate('/posts')}
+          >
+            Posts
+          </Button>
+          <Button
+            size="small"
+            variant={pathname === '/posts/2' ? 'contained' : 'text'}
+            onClick={() => handleNavigate('/posts/2')}
+          >
+            Post
+          </Button>
+          <Button
+            size="small"
+            variant={pathname === '/posts/2/edit' ? 'contained' : 'text'}
+            onClick={() => handleNavigate('/posts/2/edit')}
+          >
+            Post edit
+          </Button>
+        </Stack>
+
+        <Stack marginLeft={3} spacing={1} direction="row">
+          <NotificationsIconButton />
+          <ProfileMenu />
+          <ThemeButton />
+        </Stack>
       </Toolbar>
     </AppBar>
   );
