27184d927c6d0b995fa8e7e76565aba9cceb0857
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-18 07:56:06 +0300

    fix: posts-page

--- src/pages/posts-page/posts-page.tsx
+++ src/pages/posts-page/posts-page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Content from '../../components/content';
+import PostsLayout from '../../layouts/posts-layout';
 
 export default function PostsPage() {
   return (
     <Content header>
-      Posts
+      <PostsLayout />
     </Content>
   );
 }
