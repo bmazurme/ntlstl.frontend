2535da33dc94691b2b55e62a2d6b35d8212e16de
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-19 16:24:16 +0300

    feat: profile-page

---
+++ src/pages/profile-page/profile-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Content from '../../components/content';
+import ProfileLayout from '../../layouts/profile-layout';
+
+export default function ProfilePage() {
+  return (
+    <Content header>
+      <ProfileLayout />
+    </Content>
+  );
+}
