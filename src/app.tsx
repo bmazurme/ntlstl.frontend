86078809eaa6901c8b162a8d4e6cefde27e863bb
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-20 15:30:57 +0300

    fix: app

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-import MainPage from './pages/main';
-import NotFoundPage from './pages/not-found';
+import MainPage from './pages/main-page';
+import ProfilePage from './pages/profile-page';
+import PostsPage from './pages/posts-page';
+import PostPage from './pages/post-page';
+import PostAddPage from './pages/post-add-page';
+import PostEditPage from './pages/post-edit-page';
+import NotFoundPage from './pages/not-found-page';
 
 import { ThemeContext, DrawerContext } from './context';
 import { useDarkTheme, useDrawer } from './hooks';
@@ -25,6 +30,12 @@ export default function MiniDrawer() {
         <ThemeProvider theme={theme}>
           <Routes>
             <Route path="/" element={(<MainPage />)} />
+            <Route path="/profile/:id" element={(<ProfilePage />)} />
+            <Route path="/posts" element={(<PostsPage />)} />
+            <Route path="/posts/:page" element={(<PostsPage />)} />
+            <Route path="/post/add" element={(<PostAddPage />)} />
+            <Route path="/post/:id" element={(<PostPage />)} />
+            <Route path="/post/:id/edit" element={(<PostEditPage />)} />
             <Route path="*" element={(<NotFoundPage />)} />
           </Routes>
         </ThemeProvider>
