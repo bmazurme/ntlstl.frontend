a50d776f2608f31582c63f5e3fbc728550a61ca4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-20 15:30:20 +0300

    fix: post-edit-page

--- src/pages/post-edit-page/post-edit-page.tsx
+++ src/pages/post-edit-page/post-edit-page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Content from '../../components/content';
+import PostEditLayout from '../../layouts/post-edit-layout';
 
 export default function PostEditPage() {
   return (
     <Content header>
-      PostEdit
+      <PostEditLayout />
     </Content>
   );
 }
