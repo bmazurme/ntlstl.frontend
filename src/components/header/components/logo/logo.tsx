83356bbe213fe4353406ba7f3cb89709a659b554
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-21 00:45:22 +0300

    fix: logo

--- src/components/header/components/logo/logo.tsx
+++ src/components/header/components/logo/logo.tsx
@@ -3,16 +3,20 @@ import { useNavigate } from 'react-router-dom';
 
 import { Box, Button } from '@mui/material';
 
+import { logoStyle } from './logo-style';
+
 export default function Logo() {
   const navigate = useNavigate();
   const onHandleNavigate = () => navigate('/');
 
   return (
-    <Box
-      sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-    >
-      <Button onClick={onHandleNavigate} variant="text">NTLSTL</Button>
+    <Box sx={logoStyle}>
+      <Button
+        variant="text"
+        onClick={onHandleNavigate}
+      >
+        NTLSTL
+      </Button>
     </Box>
-
   );
 }
