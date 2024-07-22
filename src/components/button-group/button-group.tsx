3b9622a58124b7d2ce6ba718303733122428d690
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-22 12:07:27 +0300

    fix: button-group

--- src/components/button-group/button-group.tsx
+++ src/components/button-group/button-group.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import { Button, Stack } from '@mui/material';
 
 export default function ButtonGroup() {
   return (
