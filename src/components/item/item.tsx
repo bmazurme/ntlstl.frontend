fac122fcffb786baa7d54ef4d75a53bf08e4530e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-22 12:07:43 +0300

    feat: item

---
+++ src/components/item/item.tsx
@@ -0,0 +1,9 @@
+import { Paper } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  color: theme.palette.text.secondary,
+}));
